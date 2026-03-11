d23e40da5b97501109f9d5d4a082edf37bb103f2
hlovisa4 <lovisahamback@gmail.com>
2026-03-11 15:55:39 +0100

    la till PCT

--- lidralignment.R
+++ lidralignment.R
@@ -3,8 +3,8 @@ library(lidR)
 library(lidRalignment)
 options(lidR.progress = FALSE)
 
-fref = "C:/Users/digit/Downloads/Examensarbete/Results/trees_lidr/lidr_segmentation__instance_103__n5695__score_103.las"
-fmov = "C:/Users/digit/Downloads/Examensarbete/Results/trees_TLS_labeled_from_ALS_lidr_260304_clipped/TLS_labeled_from_ALS_lidr_260304_clipped__instance_103__n4076.las"
+fref = "C:/Users/digit/Downloads/Examensarbete/Results/inst_39_ALS.las"
+fmov = "C:/Users/digit/Downloads/Examensarbete/Results/inst_39_TLS.las"
 
 # Setup the pipeline. It is important to tell the object
 # what we are aligning in order to perform or not
@@ -12,7 +12,8 @@ fmov = "C:/Users/digit/Downloads/Examensarbete/Results/trees_TLS_labeled_from_AL
 alignment = AlignmentScene$new(fref, fmov)
 alignment$set_ref_is_ground_based(FALSE)
 alignment$set_mov_is_ground_based(TRUE)
-alignment$set_radius(20)
+alignment$set_radius(5)
+
 
 # Run the alignment pipeline
 alignment$align()
@@ -21,13 +22,14 @@ alignment$align()
 alignment$plot("raw")
 alignment$plot("coarse")
 alignment$plot("fine")
-alignment$extra_fine_align()
-alignment$plot("extra", compare_to = "fine")
+#alignment$extra_fine_align()
+#alignment$plot("extra", compare_to = "fine")
 
 # Get the final transformation matrix to register the entire point cloud.
 M = alignment$get_registration_matrix()
 
 crs = sf::st_crs(readLASheader(fref))
+crs = sf::st_crs(3006) 
 ofile = transform_las(fmov, M, crs)
 
 writeLAS(ofile, file = "C:/Users/digit/Downloads/Examensarbete/Results/instance103_aligned_lidrseg_TLS_clipped.las" )
