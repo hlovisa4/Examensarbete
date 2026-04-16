5822059ee22972239fc711fd9ea65e26b5088da1
hlovisa4 <lovisahamback@gmail.com>
2026-04-16 14:17:15 +0200

    sap csv generation

--- 5.SensorAnalysis/get_sap_dataframe.py
+++ 5.SensorAnalysis/get_sap_dataframe.py
@@ -10,11 +10,11 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 from shapely.geometry import Point
 
-metadata = pd.read_csv("sapflow_sensors_20250925.csv") 
+metadata = pd.read_csv("C:/Users/Lovisa/Downloads/Examensarbete/5.SensorAnalysis/sapflow_sensors_20250925.csv") 
 geometry = [Point(xy) for xy in zip(metadata["Easting"], metadata["Northing"])]
 
 #Plocka ut rätt biomassa
-ref_gdf = gpd.read_file("segmentation_match.gpkg", driver="GPKG") 
+ref_gdf = gpd.read_file("C:/Users/Lovisa/Downloads/Examensarbete/5.SensorAnalysis/ff3d_matched_trees_with_biomass.gpkg", driver="GPKG") 
 
 sap_gdf = gpd.GeoDataFrame(
     {"metadata_id": metadata["Name"]}, 
@@ -23,15 +23,15 @@ sap_gdf = gpd.GeoDataFrame(
 )
 
 nearest = gpd.sjoin_nearest(sap_gdf, ref_gdf, how="left", distance_col="dist")
-id_mapping = dict(zip(nearest["metadata_id"], nearest["ff3d_id"]))
+id_mapping = {str(k): v for k, v in dict(zip(nearest["metadata_id"], nearest["ff3d_id"])).items()}
 print(id_mapping)
 
 
 
 
-BM_df = pd.read_csv("biomass_height_distribution_summary.csv")
+BM_df = pd.read_csv("C:/Users/Lovisa/Downloads/Examensarbete/5.SensorAnalysis/biomass_height_distribution_summary.csv")
 matched_ids = set(id_mapping.values())
-filtered_BM_data = BM_df[BM_df["treeid"].isin(matched_ids)].copy()
+filtered_BM_data = BM_df[BM_df["id"].isin(matched_ids)].copy()
 print(f"Matched rows: {len(filtered_BM_data)}")
 
 
@@ -50,30 +50,37 @@ height_map = {0: 14, 1: 8, 2: 1.3}
 all_data = []
 
 for site_id in refs:
-    sap_df = pd.read_csv(f"TOA5_{site_id}_Sept_22_2025")
+    sap_df = pd.read_csv(f"C:/Users/Lovisa/Downloads/Examensarbete/5.SensorAnalysis/TOA5_{site_id}_Sept22_2025.txt")
     sap_df["TIMESTAMP"] = pd.to_datetime(sap_df["TIMESTAMP"])
     sap_df = sap_df[sap_df["TIMESTAMP"] >= "2025-05-01"] #sortera bort de innan 2025-05-01 eller vad det än är
-    
     current_ref = refs[site_id]
     for tree_num, data in current_ref.items():
-        tree_id = data[0]
+        tree_id = str(data[0])
         species = data[1]
         sensor_cols = data[2]
         ff3d_id = id_mapping[tree_id]
-        BM = filtered_BM_data.loc[filtered_BM_data["treeID"] == ff3d_id, ["0-1.3m", "1.3-8m", "8-14m", "14+m"]]
+        cols = ["0-1.3m", "1.3-8m", "8-14m", "14+m"]
+        BM = filtered_BM_data.loc[filtered_BM_data["id"] == ff3d_id, cols]
+        if BM.empty:
+            print(f"Skipping {tree_id} (ff3d_id: {ff3d_id}): No biomass data found.")
+            continue
         for idx, sensor in enumerate(sensor_cols):
-            biomass = BM[idx+1]
+            biomass = BM[cols[idx]].iloc[0]
             temp_df = pd.DataFrame({
+                "timestamp": sap_df["TIMESTAMP"],
                 "WC": sap_df[sensor],
                 "tree": tree_id,
                 "species": species,
                 "height": height_map[idx],
-                "sensor_type": sensor ,
+                "sensor_type": sensor,
                 "biomass" : biomass
             })
+            initial_len = len(temp_df)
+            temp_df = temp_df.dropna(subset=["WC"])
             all_data.append(temp_df)
         
 output = pd.concat(all_data, ignore_index=True) 
+output.to_csv("C:/Users/Lovisa/Downloads/Examensarbete/5.SensorAnalysis/sapanalysis.csv", index=False)
     
 
 
