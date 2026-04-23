1ed17c3a728012df26ddc9c54a91daa4aea4e427
hlovisa4 <lovisahamback@gmail.com>
2026-04-23 20:22:21 +0200

    new Preprocessing

--- 1.Preprocessing/preprocess_ALS_TLS.py
+++ 1.Preprocessing/preprocess_ALS_TLS.py
@@ -1,36 +1,32 @@
+import os
 import laspy
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import MultiPoint
-import shapely.vectorized as sv
-
+from shapely.geometry import Polygon
+from shapely import contains_xy
 
 # Read gpkg
-gdf = gpd.read_file("your_file.gpkg")
-
-# Reproject to a CRS with meters (important!)
-gdf = gdf.to_crs(3006)  # SWEREF99 TM (Sweden)
-
+gdf = gpd.read_file("C:/Users/Lovisa/Downloads/Examensarbete/5.SensorAnalysis/ff3d_matched_trees_with_biomass.gpkg")
+gdf = gdf.to_crs(3006)
 # Create convex hull
-hull = gdf.unary_union.convex_hull
+hull = gdf.geometry.union_all().convex_hull
 
 # Buffer 10 meters
-poly_10m = gpd.GeoDataFrame(geometry=[hull.buffer(10)], crs=gdf.crs)
-
-# Save
-poly_10m.to_file("output_polygon.gpkg", driver="GPKG")
-
+poly_10m = gpd.GeoDataFrame(geometry=[hull.buffer(5)], crs=gdf.crs)
+poly_10m.to_file("C:/Users/Lovisa/Downloads/Examensarbete/5.SensorAnalysis/reflist_polygon.gpkg", driver="GPKG")
+poly_geom = poly_10m.geometry.iloc[0]
 
 
 ###ALS 
-als_path = "C:/Users/digit/Downloads/Examensarbete/Data"
+als_path = "C:/Users/Lovisa/Downloads/240829_ALS_Matrice300_Svb/240829_ALS_Matrice300_Svb.las"
 als = laspy.read(als_path)
 
-mask = sv.contains(poly_10m, als.x, als.y)
+mask = contains_xy(poly_geom, np.asarray(als.x), np.asarray(als.y))
 
 out = laspy.LasData(als.header)
 out.points = als.points[mask]
-out_path = "C:/Users/digit/Downloads/Examensarbete/Data/ALS_clipped.las"
+out_path = "C:/Users/Lovisa/Downloads/ALS_clipped.las"
 out.write(out_path)
 print("Done: ALS clipped with convex hull + 1m buffer")
 
--- 4.seg_evaluation/vis_100_random.py
+++ 4.seg_evaluation/vis_100_random.py
@@ -3,21 +3,33 @@ import random
 import laspy
 import numpy as np
 import matplotlib.pyplot as plt
+import geopandas as gpd
 
 plt.switch_backend("Agg")  # headless
 
 input_folder = "C:/Users/digit/Downloads/Examensarbete/ff3d_tls_als_trees"
 output_folder = "C:/Users/digit/Downloads/Examensarbete/Results/TLS_ALSff3d_visualization_100_random"
 os.makedirs(output_folder, exist_ok=True)
+gdf = gpd.read_file("C:/Users/digit/Downloads/Examensarbete/Results/matched_trees.gpkg")
+filtered_gdf = gdf[gdf['ff3d_id'].notna() & gdf['chm_id'].notna()]
+random_sample = filtered_gdf.sample(n=100, random_state=42)
+sample_ids = set(random_sample['ff3d_id'].astype(str))
 
 def las_to_np(file_path):
     las = laspy.read(file_path)
     return np.vstack((las.x, las.y, las.z)).T
 
-n_samples = 100
 las_files = [f for f in os.listdir(input_folder) if f.lower().endswith(".las")]
+matched_files = [
+    f for f in las_files 
+    if any(f"i_{id_val}" in f for id_val in sample_ids)
+]
+
+print(f"Found {len(matched_files)} matching .las files.")
+
+
 random.seed(42) 
-for filename in random.sample(las_files, min(n_samples, len(las_files))):
+for filename in matched_files:
     pts = las_to_np(os.path.join(input_folder, filename))
     print(pts[2])
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
